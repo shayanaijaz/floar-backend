be90d3aba4eb9d346bbef8a3aff9dc84e68968e3
BrodyZak <31828072+BrodyZak@users.noreply.github.com>
2022-02-05 10:24:57 -0600

    Got teams route working

--- server.js
+++ server.js
@@ -1,12 +1,110 @@
+// packages needed 
 const express = require('express')
+const request = require('request')
+const bodyparser = require('body-parser')
+const { MongoClient } = require('mongodb')
+const { all } = require('proxy-addr')
+const { type } = require('os')
+
+// init app and database 
 const app = express()
-
+app.use(bodyparser.urlencoded({ extended: true }))  
 app.use(express.static("public"))
+const url = "mongodb+srv://floar-admin:floarpass@cluster0.t2uu7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const mongo = new MongoClient(url)
+mongo.connect(); 
 
+// basic test route to check if we can talk to API 
 app.get('/', (req, res) => {
-  res.send('The birth of Floar!!!!!!')
+  var options = {
+    method: 'GET',
+    url: 'https://api-nba-v1.p.rapidapi.com/teams/city/Atlanta',
+    headers: {
+      'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+      'x-rapidapi-key': '71b806f988msh6c00084b722b3a3p17923cjsnee5cd7d8c0af',
+      useQueryString: true
+    }
+  }
+
+  request(options, function (error, response, body) {
+	  if (error) throw new Error(error);
+
+    console.log("Response recieved. \n")
+
+    // grab specific info about team and add to console 
+    JsonOutput = JSON.parse(body)
+    console.log(JsonOutput.api.teams)
+    console.log("----------")
+    testJson = JsonOutput.api.teams[0].fullName
+    console.log(testJson)
+  })
+  // placeholder website page 
+  res.send('Floar Server')
 })
 
+// pull all TEAMS
+app.get('/teams', (req, res) => {
+  var gamesoptions = { 
+    method: 'GET',
+    url: 'https://api-nba-v1.p.rapidapi.com/teams/league/standard',
+    headers: {
+      'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+      'x-rapidapi-key': '71b806f988msh6c00084b722b3a3p17923cjsnee5cd7d8c0af',
+      useQueryString: true
+    }
+  }
+
+  request(gamesoptions, function (error, response, body) {
+	  if (error) throw new Error(error);
+
+    console.log("Response recieved. \n")
+
+    // grab specific info about team and add to console 
+    JsonOutput = JSON.parse(body)
+    var teams = JsonOutput.api.teams
+    console.log(JsonOutput.api.teams)
+    res.send(teams)
+  })
+})
+
+// get info about games from a specific year 
+app.get('/games', (req, res) => {
+  var gamesoptions = { 
+    method: 'GET',
+    url: 'https://api-nba-v1.p.rapidapi.com/games/seasonYear/2020',
+    headers: {
+      'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+      'x-rapidapi-key': '71b806f988msh6c00084b722b3a3p17923cjsnee5cd7d8c0af',
+      useQueryString: true
+    }
+  }
+
+  request(gamesoptions, function (error, response, body) {
+	  if (error) throw new Error(error);
+
+    console.log("Response recieved. \n")
+
+    // grab specific info about team and add to console 
+    JsonOutput = JSON.parse(body)
+    allGames = JsonOutput.api.games
+    // console.log(allGames)
+    console.log(allGames[0].city)
+
+
+
+    // loop through all games, print ones that match 
+    for (var i=0; i<allGames.length; i++){
+      var game = allGames[i]
+      if (game.city == "Atlanta") {
+        console.log(i)
+      }
+    }
+  })
+  // placeholder website page 
+  res.send('Floar Games')
+})
+
+// start server
 app.listen(process.env.PORT || 3000, () => {
   console.log(`server running...`)
 })
